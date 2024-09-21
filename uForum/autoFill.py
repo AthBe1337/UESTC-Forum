a27c4456918b85a0cc9c66933b79efa25c2177ec
ZhaoMengda <126739764+AthBe1337@users.noreply.github.com>
2024-09-22 01:49:09 +0800

    Add files via upload

---
+++ uForum/autoFill.py
@@ -0,0 +1,72 @@
+# utf-8
+from selenium import webdriver
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time,datetime
+import json
+
+
+def start(fID, sName, sID, stime):
+    #指定目标地址
+    url = "https://docs.qq.com/form/page/" + fID
+
+    #初始化浏览器，登陆账号
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(options=options)
+    #driver.get("https://docs.qq.com")
+    driver.get(url)
+    driver.delete_all_cookies()
+
+    with open('tenDocsCookies.json', 'r') as f:
+        cookies = json.load(f)
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+
+    driver.refresh()
+    driver.implicitly_wait(2)
+
+    #确定开始抢票的时间，准备开始
+    clock = stime.split(":")
+    currentTime = datetime.datetime.now()
+    executeTime = datetime.datetime(currentTime.year, currentTime.month, currentTime.day, int(clock[0]), int(clock[1]), 0)
+    waitPeriod = executeTime - currentTime
+    time.sleep(waitPeriod.seconds)
+
+    #计时结束，开始抢票
+    while datetime.datetime.now() < executeTime:
+        time.sleep(1)
+
+    driver.get(url)
+    driver.implicitly_wait(2)
+    start = time.time()
+
+    # 等待元素出现
+    timeout = 10  # 设置超时时间，单位为秒
+    locator = (By.XPATH, "//textarea[@placeholder='请输入']")
+    elements = []
+    while not elements:
+        try:
+            elements = WebDriverWait(driver, timeout).until(EC.presence_of_all_elements_located(locator))
+        except:
+            pass
+    if elements.__len__() == 0:
+        print("未找到元素")
+        return
+    elements[0].send_keys(sName)
+    elements[1].send_keys(sID)
+
+    button = driver.find_element(By.XPATH, "//button[text()='提交']")
+    #driver.execute_script("arguments[0].click();", button)
+    button.click()
+    driver.implicitly_wait(1)
+    locator = (By.XPATH, "//button[contains(.,'确认')]")
+    button = WebDriverWait(driver, timeout).until(EC.presence_of_element_located(locator))
+    button.click()
+
+    print("抢票用时：")
+    print(time.time() - start)
+    driver.quit()
+    return
---
+++ uForum/initializer.py
@@ -0,0 +1,52 @@
+# utf-8
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json
+
+
+def init():
+    url = "https://docs.qq.com/"
+    try:
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+        driver.delete_all_cookies()
+        driver.refresh()
+        driver.implicitly_wait(1)
+        elmet = driver.find_element(By.XPATH, "/html/body/div[2]/div[1]/header/div/div[4]/button[2]")
+        elmet.click()
+        driver.implicitly_wait(1)
+        elmet = driver.find_element(By.ID, "duiCheckboxId-3")
+        elmet.click()
+        driver.implicitly_wait(1)
+        driver._switch_to.frame(driver.find_element(By.XPATH, "/html/body/div[6]/div/div/div[2]/div[2]/div/div[1]/div[2]/div[1]/div/div/iframe"))
+        elmet = driver.find_element(By.XPATH, "/html/body/div[2]/div[1]/div[1]/div/div/div[2]/div[1]/img")
+        qrCodeSrc = elmet.get_attribute("src")
+        print("二维码地址如下，请尽快扫码 \n" + qrCodeSrc + "\n")
+
+        while True:
+            ch = input("是否已登录/已过期？(y/n)")
+            if(ch == 'y'):
+                break
+                pass
+            elif(ch == 'n'):
+                elmet.click()
+                driver.implicitly_wait(1)
+                qrCodeSrc = elmet.get_attribute("src")
+                print("新的二维码地址如下，请尽快扫码 \n" + qrCodeSrc + "\n")
+
+        driver.switch_to.default_content()
+        cookies = driver.get_cookies()
+
+        with open('tenDocsCookies.json', 'w') as f:
+            json.dump(cookies, f)
+            f.close()
+
+        driver.close()
+
+    except Exception as e:
+        print(e)
+        return False
+
+    return True
