3a9ef40259c6c067899aed8040985f99f6073766
AthBe1337 <126739764+AthBe1337@users.noreply.github.com>
2024-09-26 23:11:15 +0800

    Update autoFill.py

--- uForum/autoFill.py
+++ uForum/autoFill.py
@@ -8,65 +8,76 @@ import time,datetime
 import json
 
 
-def start(fID, sName, sID, stime):
-    #指定目标地址
-    url = "https://docs.qq.com/form/page/" + fID
+def start(num, target, content, execTime):
 
-    #初始化浏览器，登陆账号
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(options=options)
-    #driver.get("https://docs.qq.com")
-    driver.get(url)
-    driver.delete_all_cookies()
+    try:
+        #指定目标地址
+        url = "https://docs.qq.com/form/page/" + target
 
-    with open('tenDocsCookies.json', 'r') as f:
-        cookies = json.load(f)
-        for cookie in cookies:
-            driver.add_cookie(cookie)
+        #初始化浏览器，登陆账号
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        driver = webdriver.Chrome(options=options)
+        #driver.get("https://docs.qq.com")
+        driver.get(url)
+        driver.delete_all_cookies()
 
-    driver.refresh()
-    driver.implicitly_wait(2)
+        with open('tenDocsCookies.json', 'r') as f:
+            cookies = json.load(f)
+            for cookie in cookies:
+                driver.add_cookie(cookie)
 
-    #确定开始抢票的时间，准备开始
-    clock = stime.split(":")
-    currentTime = datetime.datetime.now()
-    executeTime = datetime.datetime(currentTime.year, currentTime.month, currentTime.day, int(clock[0]), int(clock[1]), 0)
-    waitPeriod = executeTime - currentTime
-    time.sleep(waitPeriod.seconds)
+        driver.refresh()
+        driver.implicitly_wait(2)
 
-    #计时结束，开始抢票
-    while datetime.datetime.now() < executeTime:
-        time.sleep(1)
+        #确定开始抢票的时间，准备开始
+        print("正在开始...")
+        clock = execTime.split(":")
+        currentTime = datetime.datetime.now()
+        executeTime = datetime.datetime(currentTime.year, currentTime.month, currentTime.day, int(clock[0]), int(clock[1]), 0)
+        waitPeriod = executeTime - currentTime
+        time.sleep(waitPeriod.seconds)
 
-    driver.get(url)
-    driver.implicitly_wait(2)
-    start = time.time()
+        #计时结束，开始抢票
+        while datetime.datetime.now() < executeTime:
+            time.sleep(1)
 
-    # 等待元素出现
-    timeout = 10  # 设置超时时间，单位为秒
-    locator = (By.XPATH, "//textarea[@placeholder='请输入']")
-    elements = []
-    while not elements:
-        try:
-            elements = WebDriverWait(driver, timeout).until(EC.presence_of_all_elements_located(locator))
-        except:
-            pass
-    if elements.__len__() == 0:
-        print("未找到元素")
-        return
-    elements[0].send_keys(sName)
-    elements[1].send_keys(sID)
+        start = time.time()
+        driver.get(url)
+        driver.implicitly_wait(0.5)
 
-    button = driver.find_element(By.XPATH, "//button[text()='提交']")
-    #driver.execute_script("arguments[0].click();", button)
-    button.click()
-    driver.implicitly_wait(1)
-    locator = (By.XPATH, "//button[contains(.,'确认')]")
-    button = WebDriverWait(driver, timeout).until(EC.presence_of_element_located(locator))
-    button.click()
+        # 等待元素出现
+        timeout = 10  # 设置超时时间，单位为秒
+        locator = (By.XPATH, "//textarea[@placeholder='请输入']")
+        elements = []
+        while not elements:
+            try:
+                elements = WebDriverWait(driver, timeout).until(EC.presence_of_all_elements_located(locator))
+            except:
+                pass
+        if elements.__len__() == 0:
+            print("未找到元素")
+            return
+        #elements[0].send_keys(sName)
+        #elements[1].send_keys(sID)
+        if elements.__len__() == num:
+            for i in range(num):
+                elements[i].send_keys(content[i])
+        else :
+            return False
 
-    print("抢票用时：")
-    print(time.time() - start)
-    driver.quit()
-    return
+        button = driver.find_element(By.XPATH, "//button[text()='提交']")
+        #driver.execute_script("arguments[0].click();", button)
+        button.click()
+        driver.implicitly_wait(1)
+        locator = (By.XPATH, "//button[contains(.,'确认')]")
+        button = WebDriverWait(driver, timeout).until(EC.presence_of_element_located(locator))
+        button.click()
+
+        print("抢票用时：")
+        print(time.time() - start)
+        driver.quit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
